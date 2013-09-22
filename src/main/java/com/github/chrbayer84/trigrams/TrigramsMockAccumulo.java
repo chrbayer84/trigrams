7603535872bca52b566a26f9593103764a078df1
christian <christian@t530>
2013-09-21 20:23:36 -0400

    ingest working

--- src/main/java/com/github/chrbayer84/trigrams/TrigramsMockAccumulo.java
+++ src/main/java/com/github/chrbayer84/trigrams/TrigramsMockAccumulo.java
@@ -1,5 +1,6 @@
 package com.github.chrbayer84.trigrams;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
@@ -7,15 +8,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.BatchWriterConfig;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.Instance;
+import org.apache.accumulo.core.client.IteratorSetting;
 import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.TableExistsException;
 import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.mock.MockInstance;
 import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
@@ -23,6 +25,8 @@ import org.apache.hadoop.io.Text;
 
 public class TrigramsMockAccumulo
 {
+    public static final String KEY_SEPERATOR = "|";
+
     public static void main(String[] args) throws IOException,
             AccumuloException, AccumuloSecurityException, TableExistsException,
             TableNotFoundException
@@ -31,27 +35,19 @@ public class TrigramsMockAccumulo
         Connector connector = mock.getConnector("root", "password");
         connector.tableOperations().create("TRIGRAMS");
 
-        new IngestTextFile().readFromTextFile(connector, "TRIGRAMS",
-                new BatchWriterConfig().setMaxLatency(10, TimeUnit.SECONDS)
-                        .setMaxMemory(10000000).setMaxWriteThreads(5));
-
-        Mutation m = new Mutation(new Text("john"));
-        m.put("info:name", "", "john henry");
-        m.put("info:gender", "", "male");
-        m.put("friend:old", "mark", "");
-        wr.addMutation(m);
-        m = new Mutation(new Text("mary"));
-        m.put("info:name", "", "mark wiggins");
-        m.put("info:gender", "", "female");
-        m.put("friend:new", "mark", "");
-        m.put("friend:old", "lucas", "");
-        m.put("friend:old", "aaron", "");
-        wr.addMutation(m);
-        wr.close();
-
+        BatchWriter writer = connector.createBatchWriter("TRIGRAMS",
+                new BatchWriterConfig()                        );
+        // ingest data from file
+        File file = new File( "src/main/resources/sawyer.txt");
+        new TripleIngestTextFile().readFromTextFile(file, writer);
+        writer.close();
+        
         Scanner scanner = connector.createScanner("TABLEA",
                 new Authorizations());
-        scanner.setRange(new Range("a", "z"));
+        scanner.setRange(new Range());
+        scanner.addScanIterator( new IteratorSetting(15, "randomStartIterator", iteratorClass);
+        
+        
         scanner.fetchColumnFamily(new Text("friend:old"));
         Iterator<Map.Entry<Key, Value>> iterator = scanner.iterator();
         while (iterator.hasNext())
