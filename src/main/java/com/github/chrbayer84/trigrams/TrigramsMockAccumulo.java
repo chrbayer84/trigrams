6c60e22a38f6865feceb73dadc20f0e7d685e052
christian <christian@t530>
2013-09-26 22:12:38 -0400

    - added N-gram support

--- src/main/java/com/github/chrbayer84/trigrams/TrigramsMockAccumulo.java
+++ src/main/java/com/github/chrbayer84/trigrams/TrigramsMockAccumulo.java
@@ -40,6 +40,8 @@ public class TrigramsMockAccumulo
 
     static final Random RANDOM = new Random(System.currentTimeMillis());
 
+    private static final int N = 3;
+
     public static void main(String[] args) throws IOException,
             AccumuloException, AccumuloSecurityException, TableExistsException,
             TableNotFoundException
@@ -58,7 +60,7 @@ public class TrigramsMockAccumulo
         // ingest data from file
         File file = new File("src/main/resources/sawyer.txt");
         Files.readLines(file, Charsets.UTF_8,
-                new StringLineProcessor(3, writer));
+                new StringLineProcessor(N, writer));
         writer.close();
 
         Scanner weightScanner = connector.createScanner("TRIGRAMS",
@@ -77,8 +79,6 @@ public class TrigramsMockAccumulo
         {
             Scanner scanner = connector.createScanner("TRIGRAMS",
                     new Authorizations());
-            String[] keyComponents = KEY_SEPERATOR_PATTERN.split(nextKey
-                    .getRow().toString());
             // why does that not work?
             Key endKey = new Key(Range.followingPrefix(nextKey.getRow()));
             scanner.setRange(new Range(nextKey, null));
@@ -101,10 +101,15 @@ public class TrigramsMockAccumulo
                 // could not find any keys with the specified trigram
                 return;
             }
-            keyComponents = KEY_SEPERATOR_PATTERN.split(currentKey.getRow()
-                    .toString());
-            nextKey = new Key(keyComponents[1]
-                    + TrigramsMockAccumulo.KEY_SEPARATOR
+            String[] keyComponents = KEY_SEPERATOR_PATTERN.split(currentKey
+                    .getRow().toString());
+            StringBuilder nextKeyBuilder = new StringBuilder();
+            for (int keyIndex = 1; keyIndex < N - 1; keyIndex++)
+            {
+                nextKeyBuilder.append(keyComponents[keyIndex]);
+                nextKeyBuilder.append(TrigramsMockAccumulo.KEY_SEPARATOR);
+            }
+            nextKey = new Key(nextKeyBuilder.toString()
                     + currentKey.getColumnFamily());
             // print value to console
             String nextWord = URLDecoder.decode(currentKey.getColumnFamily()
